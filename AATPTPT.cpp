2daa0b13fc047653694b25f4ac2fae9eeb7f52b0
Hüseyin Tuğrul büyükışık <huseyin.tugrul.buyukisik@gmail.com>
2023-08-22 22:27:33 +0300

    test1

--- AATPTPT.cpp
+++ AATPTPT.cpp
@@ -1,9 +1,13 @@
 
 #include <iostream>
+#include "vcpkg_installed/x86-windows/x86-windows/include/opencv2/opencv.hpp"
+#include "vcpkg_installed/x86-windows/x86-windows/include/opencv2/highgui.hpp"
 
 int main()
 {
     std::cout << "Hello World!\n";
+    cv::namedWindow("AATPTPT");
+    cv::waitKey(0);
     return 0;
 }
 
