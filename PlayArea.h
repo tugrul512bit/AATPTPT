c05433f45cd607b18f233333035c4b8984ebf9c4
Hüseyin Tuğrul büyükışık <huseyin.tugrul.buyukisik@gmail.com>
2023-08-26 22:16:45 +0300

    test200

--- PlayArea.h
+++ PlayArea.h
@@ -98,11 +98,16 @@ public:
                 const int id=get_global_id(0);                                 
                 unsigned int randomSeed = randomSeedIn[id];
                 float motionPossibility = randomFloat(&randomSeed);
-                unsigned char newPos = -1;                
-                if(temperatureIn[id] > motionPossibility)
-                {                    
-                    float positionPossibility = randomFloat(&randomSeed);
-                    newPos = positionPossibility/0.25f; // 0 = top, 1 = right, 2 = bot, 3 = left
+                unsigned char newPos = 0;   
+                float positionPossibility = randomFloat(&randomSeed);             
+                if(areaIn[id]>0 && temperatureIn[id] > motionPossibility)
+                {                                        
+                    newPos = 1 + (positionPossibility/0.25f); // 1 = top, 2 = right, 3 = bot, 4 = left
+                }
+                else if(areaIn[id] == 0)
+                {
+                    // 100+ means selecting which source to accept (when it is empty)
+                    newPos = 101 + (positionPossibility/0.25f); //  101= top, 102 = right, 103 = bot, 104 = left
                 }
                 randomSeedOut[id]=randomSeed;
                 targetPositionOut[id]=newPos;
@@ -126,41 +131,42 @@ public:
                 unsigned int randomSeed = randomSeedIn[id];
                 int idTop = (y - 1) * PLAY_AREA_WIDTH + x;
                 int top = (y>0 ? areaIn[idTop] : 0);
-                unsigned char targetTop = (y>0 ? targetPositionIn[idTop] : -1);
+                unsigned char targetTop = (y>0 ? targetPositionIn[idTop] : 0);
 
                 int idLeft = y * PLAY_AREA_WIDTH + (x-1);
                 int left =(x>0 ? areaIn[idLeft] : 0);                                                
-                unsigned char targetLeft =(x>0 ? targetPositionIn[idLeft] : -1);                                                
+                unsigned char targetLeft =(x>0 ? targetPositionIn[idLeft] : 0);                                                
 
                 int idRight = y * PLAY_AREA_WIDTH + (x+1);
                 int right =(x<PLAY_AREA_WIDTH-1 ? areaIn[idRight] : 0);             
-                unsigned char targetRight =(x<PLAY_AREA_WIDTH-1 ? targetPositionIn[idRight] : -1);             
+                unsigned char targetRight =(x<PLAY_AREA_WIDTH-1 ? targetPositionIn[idRight] : 0);
 
                 int center = areaIn[id];
+                unsigned char targetAccepted = targetPositionIn[id];
 
                 int idBot = (y + 1) * PLAY_AREA_WIDTH + x;
                 int bot = (y<PLAY_AREA_HEIGHT-1 ? areaIn[idBot] : 0);
-                unsigned char targetBot = (y<PLAY_AREA_HEIGHT-1 ? targetPositionIn[idBot] : -1);
+                unsigned char targetBot = (y<PLAY_AREA_HEIGHT-1 ? targetPositionIn[idBot] : 0);
 
                 if (center == 1)
                 {
                     unsigned char targetPos = targetPositionIn[id];
-                    if(targetPos == 0 && top == 0)
+                    if(targetPos == 1 && top == 0 && targetTop == 103)
                     {   
                         areaOut[id]=0;
                         temperatureOut[id]=0;
                     }
-                    else if(targetPos == 1 && right == 0)
+                    else if(targetPos == 2 && right == 0  && targetRight == 104)
                     {
                         areaOut[id]=0;
                         temperatureOut[id]=0;
                     }
-                    else if(targetPos == 2 && bot == 0)
+                    else if(targetPos == 3 && bot == 0 && targetBot == 101)
                     {
                         areaOut[id]=0;
                         temperatureOut[id]=0;
                     }
-                    else if(targetPos == 3 && left == 0)
+                    else if(targetPos == 4 && left == 0 && targetLeft == 102)
                     {
                         areaOut[id]=0;
                         temperatureOut[id]=0;
@@ -176,19 +182,19 @@ public:
                 {
                     // if top cell is filled and its going down
                     int sourceId = -1;                    
-                    if(top == 1 && targetTop == 2)
+                    if(top == 1 && targetTop == 3 && targetAccepted == 101)
                     {
                         sourceId = idTop;
                     }
-                    else if(right == 1 && targetRight == 3)
+                    else if(right == 1 && targetRight == 4  && targetAccepted == 102)
                     {
                         sourceId = idRight;
                     }
-                    else if(bot == 1 && targetBot == 0)
+                    else if(bot == 1 && targetBot == 1 && targetAccepted == 103)
                     {
                         sourceId = idBot;
                     }
-                    else if(left == 1 && targetLeft == 1)
+                    else if(left == 1 && targetLeft == 2 && targetAccepted == 104)
                     {
                         sourceId = idLeft;
                     }
@@ -219,7 +225,7 @@ public:
             _areaIn->access<int>(i) = 0;
             _temperatureIn->access<float>(i) = 0.0f;
             _randomSeedIn->access<int>(i) = i;
-            _targetPositionIn->access<unsigned char>(i) = -1;
+            _targetPositionIn->access<unsigned char>(i) = 0;
         }
     }
 
